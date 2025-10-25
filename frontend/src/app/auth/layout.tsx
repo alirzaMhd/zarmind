f14e889ac7b1cf19760142a2da38e88b48dfbf80
alirzaMhd <71058261+alirzaMhd@users.noreply.github.com>
2025-10-25 08:30:05 +0330

    fix auth design

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -5,11 +5,5 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-        {children}
-      </div>
-    </main>
-  );
+  return <>{children}</>;
 }
